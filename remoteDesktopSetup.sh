d76a0b001e1bf6b19888727e8d21a8f46a8445f6
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2023-05-18 20:59:21 +0200

    Update remoteDesktopSetup.sh

--- remoteDesktopSetup.sh
+++ remoteDesktopSetup.sh
@@ -2,13 +2,24 @@
 
 # Following instructions at https://ubuntu.com/blog/launch-ubuntu-22-04-desktop-on-google-cloud
 
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install slim
-sudo apt -y install ubuntu-desktop
+# Set autoservice restart for the script
+sudo cp /etc/needrestart/needrestart.conf /etc/needrestart/needrestart.conf.backup
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+
+sudo apt-get -y --assume-yes -qq update
+sudo apt-get -y --assume-yes -qq upgrade
+sudo apt-get -y --assume-yes -qq install ubuntu-desktop
+sudo apt-get remove --auto-remove gdm3 
+sudo apt-get -y --assume-yes -qq  install slim
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get -y install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 rm ./chrome-remote-desktop_current_amd64.deb
 
+#Restore autoservice restart config
+sudo echo /etc/needrestart/needrestart.conf.backup > /etc/needrestart/needrestart.conf
+sudo rm /etc/needrestart/needrestart.conf.backup 
+
+
 # Now the systems needs to reboot
 read -t 60 -p "The system needs to reboot. Press anykey to inmediate reboot. Otherwise, it will reboot in 60 senconds"
+sudo reboot
