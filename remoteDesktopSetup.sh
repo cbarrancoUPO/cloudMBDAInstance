8c7ecc53d04b4d73a98060d3246a173b2e9b8c32
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2023-05-18 21:28:55 +0200

    Update remoteDesktopSetup.sh

--- remoteDesktopSetup.sh
+++ remoteDesktopSetup.sh
@@ -6,13 +6,13 @@
 sudo cp /etc/needrestart/needrestart.conf /etc/needrestart/needrestart.conf.backup
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 
-sudo apt-get -y --assume-yes -q update
-sudo apt-get -y --assume-yes -q upgrade
-sudo apt-get -y --assume-yes -q install ubuntu-desktop
+sudo apt-get -y --assume-yes -qq update
+sudo apt-get -y --assume-yes -qq upgrade
+sudo apt-get -y --assume-yes -qq install ubuntu-desktop
 sudo apt-get -y -q --assume-yes remove --auto-remove gdm3 thunderbird
-sudo apt-get -y --assume-yes -q  install slim gnome-software firefox
+sudo apt-get -y --assume-yes -qq  install slim gnome-software firefox
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt-get -y install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo apt-get -y install --assume-yes -qq ./chrome-remote-desktop_current_amd64.deb
 sudo rm ./chrome-remote-desktop_current_amd64.deb
 
 #Restore autoservice restart config
