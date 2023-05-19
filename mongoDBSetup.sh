027623d4c1b955efd344eb76f066d787b49c6816
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2023-05-19 11:20:35 +0200

    Update mongoDBSetup.sh

--- mongoDBSetup.sh
+++ mongoDBSetup.sh
@@ -11,7 +11,7 @@ curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 sudo apt-get -y --assume-yes -qqq update
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y --assume-yes -qq mongodb-org
 
 # Start MongoDB
 sudo systemctl start mongod
