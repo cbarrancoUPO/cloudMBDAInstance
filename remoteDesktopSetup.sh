c5bef7a343279f220fe7988d179664cc97209d34
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2023-05-19 09:53:52 +0200

    Update remoteDesktopSetup.sh

--- remoteDesktopSetup.sh
+++ remoteDesktopSetup.sh
@@ -8,7 +8,7 @@ sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/
 
 sudo apt-get -y --assume-yes -qqq update
 sudo apt-get -y --assume-yes -qqq upgrade
-sudo apt-get -y --assume-yes -qq --no-install-recommends install ubuntu-desktop gnome-software firefox
+sudo apt-get -y --assume-yes -qq install ubuntu-desktop gnome-software firefox
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get -y install --assume-yes -qqq ./chrome-remote-desktop_current_amd64.deb
 sudo rm ./chrome-remote-desktop_current_amd64.deb
