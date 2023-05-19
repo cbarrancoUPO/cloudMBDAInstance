89925c73946f46a007bd4947db8970e4e2e0a118
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2023-05-19 10:26:06 +0200

    Update java11Setup.sh

--- java11Setup.sh
+++ java11Setup.sh
@@ -4,8 +4,11 @@ sudo apt-get -y --assume-yes -qqq update
 sudo apt-get -y --assume-yes -qqq upgrade
 sudo apt-get -y --assume-yes -qq install openjdk-8-jre-headless
 
-# Set JAVA_HOME for all users. Requieres a new login
+# Set JAVA_HOME for all users.
 
-sudo cat <<EOF > /etc/profile.d/jdk_home.sh
+sudo cat<<EOF > /etc/profile.d/jdk_home.sh
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
 EOF
+
+# Load JAVA_HOME right now
+source /etc/profile.d/jdk_home.sh
