feb81a4f2683a838c47d02c3e5c497a4a03ca8dc
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2024-05-15 13:36:06 +0200

    Update fullSetup.sh
    Verbosity added

--- fullSetup.sh
+++ fullSetup.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+echo "Starting the full setup of the MBDAInstance"
+echo "-------------------------------------------"
+
+echo "Starting Java 11 setup"
+echo "-------------------------------------------"
 source java11Setup.sh
+echo "Done Java 11 setup"
+echo "-------------------------------------------"
+
+echo "Starting MongoDb setup"
+echo "-------------------------------------------"
 source mongoDBSetup.sh
+echo "Done MongoDb setup"
+echo "-------------------------------------------"
+
+echo "Starting Docker setup"
+echo "-------------------------------------------"
 source dockerSetup.sh
+echo "Done Docker setup"
+echo "-------------------------------------------"
+
+echo "Remote Desktop setup"
+echo "-------------------------------------------"
 source remoteDesktopSetup.sh
+echo "Done Remote Desktop setup"
+echo "-------------------------------------------"
