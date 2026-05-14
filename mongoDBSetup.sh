ffb6611a7935708b676393a60b5c12ae223a6f1a
cbarrancoUPO <134000408+cbarrancoUPO@users.noreply.github.com>
2026-05-14 14:09:14 +0200

    Update mongoDBSetup.sh

--- mongoDBSetup.sh
+++ mongoDBSetup.sh
@@ -2,13 +2,13 @@
 
 # Following instructions in https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
 
-sudo apt-get -y --assume-yes -qq install gnupg
+sudo apt-get install gnupg curl
 
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+curl -fsSL https://pgp.mongodb.com/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
    --dearmor
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 
 sudo apt-get -y --assume-yes -qqq update
 sudo apt-get install -y --assume-yes -qq mongodb-org
